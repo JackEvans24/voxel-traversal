28da4e126c5f7892d157714e1fd187e374af0b9a
Jevans (Mac) <jackevans24@gmail.com>
2025-09-12 13:32:51 +0100

    feat: add camera controller

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -10,16 +10,29 @@ namespace TraversalDemo.Controller
         [SerializeField] private CellAddress gridSize;
 
         private GridVisualiser gridVisualiser;
+        private CameraController cameraController;
 
         private void Awake()
         {
             gridVisualiser = GetComponentInChildren<GridVisualiser>();
+            cameraController = AddChildComponent<CameraController>();
         }
 
         private void Start()
         {
             var gridCells = GridGenerationService.GenerateGrid(gridSize);
             gridVisualiser.UpdateGridUI(gridCells);
+            
+            cameraController.CenterCameraOnGrid(gridSize);
+        }
+
+        private T AddChildComponent<T>() where T : MonoBehaviour
+        {
+            var childGameObject = new GameObject(typeof(T).Name);
+            childGameObject.transform.parent = transform;
+
+            var component = childGameObject.AddComponent<T>();
+            return component;
         }
     }
 }
