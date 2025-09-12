c0db3c7854bad2b9dc4a5eef02fe63afd5b01cd0
Jevans (Mac) <jackevans24@gmail.com>
2025-09-12 13:15:23 +0100

    feat: service to generate grid

--- Assets/Scripts/Data/CellAddress.cs
+++ Assets/Scripts/Data/CellAddress.cs
@@ -1,14 +1,33 @@
+using System;
+using UnityEngine;
+
 namespace TraversalDemo.Data
 {
-    public struct CellAddress
+    [Serializable]
+    public struct CellAddress : IEquatable<CellAddress>
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        [SerializeField] public int x;
+        [SerializeField] public int y;
 
         public CellAddress(int x, int y)
         {
-            X = x;
-            Y = y;
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool Equals(CellAddress other)
+        {
+            return x == other.x && y == other.y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is CellAddress other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y);
         }
     }
 }
---
+++ Assets/Scripts/Data/GridCell.cs
@@ -0,0 +1,8 @@
+namespace TraversalDemo.Data
+{
+    public struct GridCell
+    {
+        public CellAddress Address { get; set; }
+        public bool IsWall { get; set; }
+    }
+}
