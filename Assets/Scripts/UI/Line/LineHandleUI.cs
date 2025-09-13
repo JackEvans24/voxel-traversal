d9dc7760e2ba7446dd2ce8f8f9922624aa562d2e
Jevans (Mac) <jackevans24@gmail.com>
2025-09-13 09:26:13 +0100

    feat: add line handle

---
+++ Assets/Scripts/UI/Line/LineHandleUI.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+namespace TraversalDemo.UI.Line
+{
+    public class LineHandleUI : MonoBehaviour
+    {
+        public event Action<Vector3> PositionUpdated;
+
+        private Camera cam;
+
+        private Vector2 offset;
+        private bool isDragging;
+
+        private void Start()
+        {
+            cam = Camera.main;
+        }
+
+        private void OnMouseDown()
+        {
+            var localMousePosition = GetLocalMousePosition();
+            offset = transform.position - localMousePosition;
+            isDragging = true;
+        }
+
+        private void OnMouseDrag()
+        {
+            if (!isDragging)
+                return;
+
+            var localMousePosition = GetLocalMousePosition();
+            transform.position = localMousePosition + (Vector3)offset;
+
+            PositionUpdated?.Invoke(transform.position);
+        }
+
+        private void OnMouseUp()
+        {
+            isDragging = false;
+        }
+
+        private Vector3 GetLocalMousePosition()
+        {
+            var localMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            localMousePosition.z = 0;
+            return localMousePosition;
+        }
+    }
+}
